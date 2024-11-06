6645728fa1a4d69c98d899322da948d935f85e36
Farooq <farooqraza401@gmail.com>
2024-11-06 17:18:07 +0530

    my first commit callback functions

---
+++ callback.js
@@ -0,0 +1,13 @@
+function greet(name, callback) {
+    console.log(`Hello, ${name}!`);
+    callback();
+  }
+  
+  function sayGoodbye() {
+    console.log("Goodbye!");
+  }
+  
+  greet("Alice", sayGoodbye); 
+
+  //In this example, sayGoodbye is a callback function passed to greet. 
+  //After greet logs the greeting, it calls callback(), which then logs "Goodbye!".
